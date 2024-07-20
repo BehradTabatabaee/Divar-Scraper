8f6a26166a388d0232bad7ba3270f3a5fa3edfb4
Behrad Tabatabaee <behradtabatabaee3@gmail.com>
2024-07-20 11:29:26 +0330

    infinite scrolling added

---
+++ scraper.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import arabic_reshaper
+from bidi.algorithm import get_display
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def convert(text):
+    reshaped_text = arabic_reshaper.reshape(text)
+    converted = get_display(reshaped_text)
+    return converted
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+
+cities = ["tehran", "mashhad", "karaj", "shiraz", "isfahan", "ahvaz", "tabriz", "kermanshah", "qom", "rasht"]
+categories = []
+dataIndexes = set()
+
+try:
+    print("Enter The Cities:")
+    for i, city in enumerate(cities, start=1):
+        print(f"{i}: {convert(city)}")
+    enteredCity = int(input())
+    driver.get(f"https://divar.ir/s/{cities[enteredCity - 1]}")
+    
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "kt-accordion-item__header")))
+    
+    categoriesTemp = driver.find_elements(by=By.CLASS_NAME, value="kt-accordion-item__header")
+    for category in categoriesTemp:
+        categories.append(category)
+    
+    print("Select a Category:")
+    for i, c in enumerate(categories, start=1):
+        categoryName = convert(c.accessible_name.partition(' ')[2])
+        print(f"{i}: {categoryName}")
+    chosenCategory = int(input()) - 1
+    categories[chosenCategory].click()
+    
+    datanum = int(input("How many?\n"))
+    loaded_indexes = 0
+    current_scroll = 0
+    while loaded_indexes < datanum:
+        try:
+            indexes = driver.find_elements(By.CSS_SELECTOR, '[data-index]')
+            for index in indexes:
+                data_index = index.get_attribute('data-index')
+                if data_index not in dataIndexes:
+                    dataIndexes.add(data_index)
+                    loaded_indexes += 1
+                    print(f"Loaded indexes: {loaded_indexes}")
+                    if loaded_indexes >= datanum:
+                        break
+
+            try:
+                load_more_button = driver.find_element(By.CLASS_NAME, value = 'post-list__load-more-btn-d46f4')
+                if load_more_button:
+                    load_more_button.click()
+                    WebDriverWait(driver, 10).until(EC.staleness_of(load_more_button))
+            except:
+                pass
+
+        except Exception as e:
+            if 'stale element reference' not in str(e):
+                print(f"An error occurred while processing elements: {e}")
+
+        current_scroll += 100
+        driver.execute_script(f"window.scrollTo(0, {current_scroll});")
+        time.sleep(0.025)  # Add a short sleep to give time for new elements to load
+
+    input()
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    print("Work finished. Closing...")
+    driver.quit()
